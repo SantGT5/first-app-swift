be9fcc638f90c60521eedbc6a6b0ef370fada3e3
jose09511 <jose09511@gmail.com>
2021-11-03 16:33:23 +0100

    first commit

---
+++ first-app-swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  first-app-swift
+//
+//  Created by Jose Manuel Barba Sánchez on 3/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hola mundo!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ first-app-swift/first_app_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_app_swiftApp.swift
+//  first-app-swift
+//
+//  Created by Jose Manuel Barba Sánchez on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct first_app_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
